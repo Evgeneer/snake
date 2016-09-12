54c4e60f8e24d8a03d431ec6cb83ffce6ff656c5
Evgeneer <alookard7@mail.ru>
2016-09-12 17:57:03 +0500

    Snake has been added

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -11,12 +11,26 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
-
         public Point(int _x, int _y, char _sym)
         {
             x = _x;
             y = _y;
             sym = _sym;
+        }
+
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT) x += offset;
+            else if (direction == Direction.LEFT) x -= offset;
+            else if (direction == Direction.UP) y += offset;
+            else if (direction == Direction.DOWN) y -= offset;
         }
 
         public void Draw()
