402a5550a242f712904272dc265782b555ec29e0
K2nK1n <alookard7@mail.ru>
2017-08-12 21:18:36 +0500

    Snake moves!

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -11,10 +12,10 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '-');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '-');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '|');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '|');
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             upLine.Draw();
             downLine.Draw();
             leftLine.Draw();
@@ -23,6 +24,24 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            snake.Move();
+
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
 
             Console.ReadLine();
         }
